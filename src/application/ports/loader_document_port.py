e0dc08c028a6b972970f5134b17b8897c24ef8b2
Henry <elpadredelcordero@gmail.com>
2025-10-03 21:36:07 -0500

    fixes

--- src/application/ports/loader_document_port.py
+++ src/application/ports/loader_document_port.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class LoaderDocumentPort(ABC):
     @abstractmethod
     def save_document(self, key: str, data: bytes) -> None:
--- src/domain/models/enums/document_type.py
+++ src/domain/models/enums/document_type.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class DocumentType(Enum):
     POLICY = "POLICY"
     APPRAISAL = "APPRAISAL"
